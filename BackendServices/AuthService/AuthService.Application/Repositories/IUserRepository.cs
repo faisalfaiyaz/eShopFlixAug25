ad06d1c92886a612db785f4cc2f78ddc2188e25e
ffmomin007 <ffmomin007@gmail.com>
2025-08-22 12:37:53 +0530

    feat(AuthService): Added Entities, DbContext, Repository implementations, and SQL scripts
    - Created entity classes for AuthService
    - Configured DbContext for database access
    - Implemented repository pattern with concrete classes
    - Added SQLScripts folder under Infrastructure/Persistence containing Schema.sql and SeedData.sql
    
    Next: Implement service layer logic in Application layer

---
+++ BackendServices/AuthService/AuthService.Application/Repositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using AuthService.Domain.Entities;
+
+namespace AuthService.Application.Repositories;
+
+public interface IUserRepository
+{
+    bool RegisterUser(User user, string role);
+    User GetUserByEmail(string email);
+    IEnumerable<User> GetAll();
+}
---
+++ BackendServices/AuthService/AuthService.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,36 @@
+﻿using AuthService.Application.Repositories;
+using AuthService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.Infrastructure.Persistence.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly AuthServiceDbContext _db;
+
+    public UserRepository(AuthServiceDbContext db)
+    {
+        _db = db;
+    }
+
+    public IEnumerable<User> GetAll()
+    {
+        return _db.Users.ToList();
+    }
+
+    public User GetUserByEmail(string email)
+    {
+        return _db.Users.Include(u => u.Roles).FirstOrDefault(u => u.Email == email)!;
+    }
+
+    public bool RegisterUser(User user, string role)
+    {
+        Role? existingRole = _db.Roles.FirstOrDefault(r => r.Name == role);
+        if (existingRole == null) return false;
+
+        user.Roles.Add(existingRole);
+        _db.Users.Add(user);
+        _db.SaveChanges();
+        return true;
+    }
+}
