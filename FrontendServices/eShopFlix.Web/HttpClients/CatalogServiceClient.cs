8331338a8643db386328881582b0753ecb19badc
ffmomin007 <ffmomin007@gmail.com>
2025-08-22 11:00:51 +0530

    feat(frontend): add ASP.NET Core MVC projects for Internal Support and Shopping Website
    - Added new ASP.NET Core MVC project for Internal Support under frontend services
    - Added Shopping Website project under frontend services
    - Displayed products on the Shopping Website
    
    next: start work on adding Auth Service as a new microservice

---
+++ FrontendServices/eShopFlix.Web/HttpClients/CatalogServiceClient.cs
@@ -0,0 +1,31 @@
+﻿using eShopFlix.Web.Models;
+using System.Text.Json;
+
+namespace eShopFlix.Web.HttpClients;
+
+public class CatalogServiceClient
+{
+    readonly HttpClient _httpClient;
+    public CatalogServiceClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<IEnumerable<ProductModel>> GetProducts()
+    {
+        HttpResponseMessage response = await _httpClient.GetAsync("/catalog/getall");
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            IEnumerable<ProductModel>? products = JsonSerializer.Deserialize<IEnumerable<ProductModel>>(content, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            });
+
+            return products;
+        }
+
+        return Enumerable.Empty<ProductModel>();
+
+    }
+}
