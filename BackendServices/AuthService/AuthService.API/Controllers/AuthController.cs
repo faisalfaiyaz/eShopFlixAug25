779b41f09e687a918a6b3e5beb499d411ee687e6
ffmomin007 <ffmomin007@gmail.com>
2025-08-23 06:56:50 +0530

    feat(AuthService): Implemented AuthController and updated Ocelot config
    - Added AuthController with login/register endpoints
    - Configured routing for AuthService in ocelot-dev.json
    
    Next: Implement HttpClient integration for AuthService

---
+++ BackendServices/AuthService/AuthService.API/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using AuthService.Application.DTOs;
+using AuthService.Application.Services.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.API.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly IUserAppService _userAppService;
+
+    public AuthController(IUserAppService userAppService)
+    {
+        _userAppService = userAppService;
+    }
+
+    [HttpPost]
+    public IActionResult Login(LoginDTO loginDTO)
+    {
+       UserDTO userDTO = _userAppService.LoginUser(loginDTO);
+       if(userDTO == null)
+       {
+            return Unauthorized("Invalid login attempt.");
+       }
+        
+       return Ok(userDTO);      
+    }
+
+
+    [HttpPost]
+    public IActionResult SignUp(SignUpDTO signUpDTO, string role)
+    {
+        bool isSignedUp = _userAppService.SignUpUser(signUpDTO, role);
+        if (!isSignedUp)
+        {
+            return BadRequest("Sign up failed. User may already exist.");
+        }
+
+        return Ok("User signed up successfully.");
+    }
+}
