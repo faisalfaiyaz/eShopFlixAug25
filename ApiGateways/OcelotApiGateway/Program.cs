6112b75fb4f16ce2965e2ef1e793c7330895878f
ffmomin007 <ffmomin007@gmail.com>
2025-09-01 09:06:54 +0530

    feat(ApiGateway): implement API security rules in ocelot-dev.json and ocelot.json.
    Configured security rules in Ocelot API Gateway (ocelot-dev.json) to enforce proper request handling and protection.
    
    Next: Start implementation of Cart Service.

--- ApiGateways/OcelotApiGateway/Program.cs
+++ ApiGateways/OcelotApiGateway/Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +16,22 @@ else
 
 // Add services to the container.
 builder.Services.AddOcelot();
+
+var endPointAuthKey = builder.Configuration["Keys:EndPointAuthKey"];
+
+builder.Services.AddAuthentication().AddJwtBearer(endPointAuthKey, options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
 
 var app = builder.Build();
 
