4799a467c04164233b9f3fd4be39c65d3c6ee5b8
ffmomin007 <ffmomin007@gmail.com>
2025-09-02 02:50:10 +0530

    feat: Implement Entities, CartServiceDbContext, and add ICartRepository in Application layer
    - Added entity classes for CartService
    - Configured CartServiceDbContext
    - Defined ICartRepository interface in Application layer
    
    Next: Add DTOs, Mappers, ICartRepository implementation, ICartAppService and its implementation

---
+++ BackendServices/CartService/CartService.Application/Repositories/ICartRepository.cs
@@ -0,0 +1,15 @@
+﻿using CartService.Domain.Entities;
+
+namespace CartService.Application.Repositories;
+
+public interface ICartRepository
+{
+    Cart GetUserCart(long UserId);
+    int GetCartItemCount(long UserId);
+    IEnumerable<CartItem> GetCartItems(long CartId);
+    Cart GetCart(long CartId);
+    Cart AddItem(long CartId, long UserId, CartItem item);
+    int DeleteItem(long CartId, int ItemId);
+    bool MakeInActive(long CartId);
+    int UpdateQuantity(long CartId, int ItemId, int Quantity);
+}
